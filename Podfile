cd638f93f57b2ae94ff1e68b424d7c1092c97b40
Shinya Nakajima <19910245+nakasho-dev@users.noreply.github.com>
2018-07-23 00:14:15 +0900

    XCUITestのテストケースを作成

--- Podfile
+++ Podfile
@@ -5,10 +5,10 @@ target 'sampleapp-ios-swift' do
   pod 'AppCenter'
   pod 'AppCenter/Push'
   
-  target 'sampleapp-ios-swiftUITests' do
-    inherit! :search_paths
-    # Pods for testing
-    pod 'VSMobileCenterExtensions'
-  end
+#  target 'sampleapp-ios-swiftUITests' do
+#    inherit! :search_paths
+#    # Pods for testing
+#    pod 'VSMobileCenterExtensions'
+#  end
 
 end
--- sampleapp-ios-swiftUITests/sampleapp_ios_swiftUITests.swift
+++ sampleapp-ios-swiftUITests/sampleapp_ios_swiftUITests.swift
@@ -1,20 +1,91 @@
 
 import XCTest
+import AppCenterXCUITestExtensions
 
 class sampleapp_ios_swiftUITests: XCTestCase {
-    let app = XCUIApplication()
+    var app: XCUIApplication!
     override func setUp() {
         super.setUp()
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        app.launch()
+//        continueAfterFailure = false
+        app = ACTLaunch.launch(XCUIApplication())
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
+    func test_viewBuildPage_swipeLeftOneTime() {
+        let window = app.windows.element(boundBy: 0)
+
+        window.swipeLeft()
+
+        XCTAssert(app.staticTexts["Build"].exists)
+    }
+
+    func test_viewTestPage_swipeLeftTwoTime() {
+        let window = app.windows.element(boundBy: 0)
+        
+        window.swipeLeft()
+        window.swipeLeft()
+
+        XCTAssert(app.staticTexts["Test"].exists)
+    }
+
+    func test_viewDistributePage_swipeLeftThreeTime() {
+        let window = app.windows.element(boundBy: 0)
+        
+        window.swipeLeft()
+        window.swipeLeft()
+        window.swipeLeft()
+
+        XCTAssert(app.staticTexts["Distribute"].exists)
+    }
+
+    func test_viewCrashesPage_swipeLeftFourTime() {
+        let window = app.windows.element(boundBy: 0)
+        
+        window.swipeLeft()
+        window.swipeLeft()
+        window.swipeLeft()
+        window.swipeLeft()
+        
+        XCTAssert(app.staticTexts["Crashes"].exists)
+    }
+
+    func test_viewCrashAlert_whenTapButtonOnCrashPage() {
+        let window = app.windows.element(boundBy: 0)
+        
+        window.swipeLeft()
+        window.swipeLeft()
+        window.swipeLeft()
+        window.swipeLeft()
+        
+        let crasheButton = app.buttons["fatalErrorButton"]
+        
+        crasheButton.tap()
+        
+        let crashAlert = app.alerts["The app will close"]
+        
+        XCTAssert(crashAlert.exists)
+    }
+    
+//    func test_crashApp_when() {
+//        let window = app.windows.element(boundBy: 0)
+//
+//        window.swipeLeft()
+//        window.swipeLeft()
+//        window.swipeLeft()
+//        window.swipeLeft()
+//
+//        let crasheButton = app.buttons["fatalErrorButton"]
+//
+//        crasheButton.tap()
+//
+//        let crashAlert = app.alerts["The app will close"]
+//
+//        crashAlert.buttons["Crash app"].tap()
+//    }
+//
     func testSwipeUntilCrashesPage() {
         // Assert that the crash button exists
         let window = app.windows.element(boundBy: 0)
